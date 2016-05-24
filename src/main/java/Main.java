a5d532e788b6a05c864af0e3c4ae074e7d3d6c22
Cesar A. Garcia <mrcesar9402@gmail.com>
2016-05-24 18:53:05 -0400

    Pruebas Freemarker

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,28 @@
+import freemarker.template.Configuration;
 import org.h2.jdbc.JdbcStatement;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
 
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
-        get("/", (req, res) -> {
-            Connection conexion = DriverManager.getConnection("jdbc:h2:~/practica5", "sa", "");
-            // add application code here
 
+
+        Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(Main.class, "/templates");
+        FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine(configuration);
+
+        get("/", (req, res) -> {
+            //bd stuff
             StringBuilder s = new StringBuilder("");
 
+            Connection conexion = DriverManager.getConnection("jdbc:h2:~/practica5", "sa", "");
             Statement stm = conexion.createStatement();
-
-            stm.executeUpdate("INSERT INTO estudiantes VALUES(00000001,'missigno','missigno','missigno')");
 
             ResultSet rs = stm.executeQuery("SELECT * FROM estudiantes");
 
@@ -24,9 +32,11 @@ public class Main {
                 s.append("<br/>" + rs.getString("nombre"));
             }
 
-            conexion.close();
+            //template stuff
+            Map<String, String> data = new HashMap<>();
+            data.put("texto_ejemplo",s.toString());
 
-            return "Inicio CRUD Estudiantes " + s.toString();
-        });
+            return new ModelAndView(data,"sample.ftl");
+        }, freeMarkerEngine);
     }
 }
---
+++ src/main/resources/templates/sample.ftl
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sample</title>
+    <link href="/css/miEstilo.css" rel="stylesheet" >
+</head>
+<body>
+<h1>${texto_ejemplo}</h1>
+</body>
+</html>
